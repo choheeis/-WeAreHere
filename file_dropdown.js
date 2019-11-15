a725e8ec1582efd054193d27e96e323e09471e68
SEONGGYU96 <link5658@gmail.com>
2019-11-15 17:26:11 +0900

    upload page is made

--- file_dropdown.js
+++ file_dropdown.js
@@ -1,68 +1,81 @@
-        function preview(file, idx) {
+var uploadFiles = [];
 
-            var reader = new FileReader();
+function preview(file, idx) {
 
-            reader.onload = (function(f, idx) {
+    var reader = new FileReader();
 
-                return function(e) {
-                    var $thum_ct = $('<div></div>');
-                    var $thum_div = $('<div></div>');
-                    var $thum_close = $('<div>X</div>');
-                    var $thum_img = $('<img>');
+    reader.onload = (function (f, idx) {
 
-                    $thum_ct.addClass('container');
-                    $thum_div.addClass('thumb');
-                    $thum_close.addClass('close');
-                    $thum_close.attr('data-idx', idx);
-                    $thum_img.attr({
-                        src: e.target.result,
-                        title: escape(f.name)
-                    });
+        return function (e) {
+            //var $thum_ct = $('<div></div>');
+            var $thum_div = $('<div></div>');
+            var $thum_close = $('<div>X</div>');
+            var $thum_img = $('<img>');
 
-                    $thum_div.append($thum_close);
-                    $thum_div.append($thum_img);
-                    $thum_ct.append($thum_div);
+            //$thum_ct.addClass('container');
+            $thum_div.addClass('thumb');
+            $thum_close.addClass('close');
+            $thum_close.attr('data-idx', idx);
+            $thum_img.attr({
+                src: e.target.result,
+                title: escape(f.name)
+            });
 
-                    $('#thumbnails').append($thum_ct);
-                    //resize($thum_div);
-                };
+            $thum_div.append($thum_close);
+            $thum_div.append($thum_img);
+            //$thum_ct.append($thum_div);
 
-            })(file, idx);
-            reader.readAsDataURL(file);
-            
-
+            //$('#thumbnails').append($thum_ct);
+            $('#thumbnails').append($thum_div);
+            resize($thum_img);
+            //isFile();
         };
-        
-        function resize(last_prv) {
-            var maxWidth = 200;
-            var maxHeight = 200;
-            var ratio = 0;
-            var width = $(last_prv).width();
-            var height = $(last_prv).height();
-            console.log('h = ' + height + ' w = ' + width);
-            
-            if(width > maxWidth) { 
-                console.log('width processing');
-                ratio = maxWidth / width;
-                //console.log('ratio = ' + ratio);
-                $(last_prv).css('width', maxWidth);
-                $(last_prv).css('height', height * ratio);
-                height = height * ratio;
-                //console.log('h = ' + $(last_prv).css('height') + ' w = ' + $(last_prv).css('width'));
-            }
-            
-            var width = $(last_prv).width();
-            var height = $(last_prv).height();
-            
-            if(height > maxHeight) {
-                console.log('height processing');
-                ratio = maxHeight / height;
-                //console.log('ratio = ' + ratio);
-                $(last_prv).css('height', maxHeight);
-                $(last_prv).css('width', width * ratio);
-                width = width * ratio;
-                //console.log('h = ' + $(last_prv).css('height') + ' w = ' + $(last_prv).css('width'));
-            }
-        }
-        
-        
+
+    })(file, idx);
+    reader.readAsDataURL(file);
+
+
+};
+
+function resize(last_prv) {
+    var maxWidth = 170;
+    var maxHeight = 170;
+    var ratio = 0;
+    var width = $(last_prv).width();
+    var height = $(last_prv).height();
+    console.log('h = ' + height + ' w = ' + width);
+
+    if (width > maxWidth) {
+        console.log('width processing');
+        ratio = maxWidth / width;
+        //console.log('ratio = ' + ratio);
+        $(last_prv).css('width', maxWidth);
+        $(last_prv).css('height', height * ratio);
+        height = height * ratio;
+        //console.log('h = ' + $(last_prv).css('height') + ' w = ' + $(last_prv).css('width'));
+    }
+
+    var width = $(last_prv).width();
+    var height = $(last_prv).height();
+
+    if (height > maxHeight) {
+        console.log('height processing');
+        ratio = maxHeight / height;
+        //console.log('ratio = ' + ratio);
+        $(last_prv).css('height', maxHeight);
+        $(last_prv).css('width', width * ratio);
+        width = width * ratio;
+        //console.log('h = ' + $(last_prv).css('height') + ' w = ' + $(last_prv).css('width'));
+    }
+}
+
+/*function isFile() {
+    if ($('#thumbnails').children().length == 0) {
+        $('#drop').attr('background-color', 'rgba(187, 255, 255, 0.56)');
+        $('#drop').html('파일을 드롭 앤 드롭하여 첨부');
+    } else {
+        $('#drop').attr('background-color', '');
+        $('#drop').html('');
+    }
+
+}*/
