0866e0060790016d1757295ad9832e01acdcab43
SEONGGYU96 <link5658@gmail.com>
2019-12-07 17:05:43 +0900

    검색 기능 완성git add * (상품 재배열 제외)

--- Script/add_data_script.js
+++ Script/add_data_script.js
@@ -6,13 +6,13 @@ var addProduct = function ($target_div, productData) { //상품 추가하는 함
     //현재 페이지에 있는 상품 행의 개수를 검사
     var numOfChildren = $target_div.children().length;
 
-    console.log("content has " + numOfChildren + " rows");
+    //console.log("content has " + numOfChildren + " rows");
 
     //상품의 행이 없다면 새로 만듦
     if (numOfChildren == 0) {
         makeRowProduct($target_div);
         numOfChildren++;
-        console.log("context don't have row. row is created.");
+        //console.log("context don't have row. row is created.");
     }
 
     //마지막 행(상품을 추가할)을 정의
@@ -21,13 +21,13 @@ var addProduct = function ($target_div, productData) { //상품 추가하는 함
     //마지막 행의 열 개수를 검사
     var numOfRowChildren = $parent_row_product.children().length;
 
-    console.log("last row has " + numOfRowChildren + " cols.");
+    //console.log("last row has " + numOfRowChildren + " cols.");
 
     //상품을 추가할 행이 가득 찼다면 새로운 행을 만듦 
     if (numOfRowChildren == COL_LENGTH) {
         makeRowProduct($target_div);
         numOfChildren++;
-        console.log("last row is full. new row is created.");
+        //console.log("last row is full. new row is created.");
     }
 
     //마지막 행을 다시 정의
@@ -36,7 +36,7 @@ var addProduct = function ($target_div, productData) { //상품 추가하는 함
     //마지막 행에 열을 추가
     makeColProduct($parent_row_product, productData);
 
-    console.log("col is created in row[" + (numOfChildren - 1) + "].");
+    //console.log("col is created in row[" + (numOfChildren - 1) + "].");
 }
 
 //열을 추가하는 함수
@@ -55,7 +55,7 @@ var makeColProduct = function ($parent_row_product, productData) {
     //네 번째 상품이면 오른쪽 마진을 없앤다
     if ($parent_row_product.children().length == COL_LENGTH - 1) {
         $col_product.css("margin-right", "0px");
-        console.log("last row has " + $parent_row_product.children().length + " cols. we need no margin col.");
+        //console.log("last row has " + $parent_row_product.children().length + " cols. we need no margin col.");
 
     }
     //요소 연결
